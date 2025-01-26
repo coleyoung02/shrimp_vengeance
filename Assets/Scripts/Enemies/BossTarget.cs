cf9b3acbe7864985995a78522a1cbed131259acc
Jad-Doggy <pandajadon@gmail.com>
2025-01-26 15:41:12 -0800

    metal pipe

--- Assets/Scripts/Enemies/BossTarget.cs
+++ Assets/Scripts/Enemies/BossTarget.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 public class BossTarget : Enemy
 {
     [SerializeField] private BossAttack boss;
+    private string eventPath = "event:/metalpipe";
     public override void Damage(float damage = 1)
     {
+        FMODUnity.RuntimeManager.PlayOneShot(eventPath);
         boss.NotifyDead();
         base.Damage(damage);
     }
